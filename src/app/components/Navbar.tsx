f326759475094bff887b36465ed5cf2bb6585ca1
brian033 <85883730+brian033@users.noreply.github.com>
2025-06-02 18:12:26 +0800

    removed links

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -34,24 +34,6 @@ export default function Navbar() {
                     <Link href="/" className="py-2 lg:py-0">
                         首頁
                     </Link>
-                    <Link href="/conference" className="py-2 lg:py-0">
-                        研討會
-                    </Link>
-                    <Link href="/speakers" className="py-2 lg:py-0">
-                        專題講者
-                    </Link>
-                    <Link href="/schedule" className="py-2 lg:py-0">
-                        大會議程
-                    </Link>
-                    <Link href="/travel" className="py-2 lg:py-0">
-                        住宿與交通
-                    </Link>
-                    <Link href="/robot" className="py-2 lg:py-0">
-                        田間機器人
-                    </Link>
-                    <Link href="/downloads" className="py-2 lg:py-0">
-                        檔案下載
-                    </Link>
                 </div>
                 {/* 佔位符 */}
                 <div className="hidden lg:block"></div>
@@ -65,23 +47,6 @@ export default function Navbar() {
             {/* Logo/標題 */}
             <div className="font-bold text-lg text-center lg:text-left flex-1 lg:flex-initial">
                 2025 農機與生機學術研討會
-            </div>
-
-            {/* 漢堡菜單按鈕與登入按鈕並排 - 只在小螢幕顯示 */}
-            <div className="lg:hidden flex items-center gap-2">
-                {!user && (
-                    <Button
-                        onClick={() => signIn("google")}
-                        variant="secondary"
-                        size="sm"
-                        className="text-xs py-1 px-2"
-                    >
-                        登入
-                    </Button>
-                )}
-                <Button variant="ghost" size="sm" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-                    {isMenuOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
-                </Button>
             </div>
 
             {/* 主菜單 - 在桌面顯示為水平排列，在移動設備上顯示為浮動菜單 */}
@@ -100,148 +65,6 @@ export default function Navbar() {
                 >
                     首頁
                 </Link>
-                <Link
-                    href="/conference"
-                    onClick={handleNavItemClick}
-                    className="py-2 lg:py-0 flex justify-end lg:mx-0 mx-10"
-                >
-                    研討會
-                </Link>
-                <Link
-                    href="/speakers"
-                    onClick={handleNavItemClick}
-                    className="py-2 lg:py-0 flex justify-end lg:mx-0 mx-10"
-                >
-                    專題講者
-                </Link>
-                <Link
-                    href="/schedule"
-                    onClick={handleNavItemClick}
-                    className="py-2 lg:py-0 flex justify-end lg:mx-0 mx-10  "
-                >
-                    大會議程
-                </Link>
-                <Link
-                    href="/travel"
-                    onClick={handleNavItemClick}
-                    className="py-2 lg:py-0 flex justify-end lg:mx-0 mx-10"
-                >
-                    住宿與交通
-                </Link>
-                <Link
-                    href="/robot"
-                    onClick={handleNavItemClick}
-                    className="py-2 lg:py-0 flex justify-end lg:mx-0 mx-10"
-                >
-                    田間機器人
-                </Link>
-                <Link
-                    href="/downloads"
-                    onClick={handleNavItemClick}
-                    className="py-2 lg:py-0 flex justify-end lg:mx-0 mx-10"
-                >
-                    檔案下載
-                </Link>
-                <div className="hidden lg:block border-l border-primary-foreground/20 h-6" />
-                {user && (
-                    <Link
-                        href="/profile"
-                        onClick={handleNavItemClick}
-                        className="py-2 lg:py-0 flex justify-end lg:mx-0 mx-10"
-                    >
-                        個人頁面
-                    </Link>
-                )}
-                {(user?.role === "reviewer" || user?.role === "admin") && (
-                    <Link
-                        href="/reviewer"
-                        onClick={handleNavItemClick}
-                        className="py-2 lg:py-0 flex justify-end lg:mx-0 mx-10"
-                    >
-                        待審介面
-                    </Link>
-                )}
-                {user?.role === "admin" && (
-                    <>
-                        <Link
-                            href="/admin/user_management"
-                            onClick={handleNavItemClick}
-                            className="py-2 lg:py-0 flex justify-end lg:mx-0 mx-10"
-                        >
-                            <Button
-                                variant="destructive"
-                                size="sm"
-                                className="w-30% lg:w-auto flex justify-end "
-                            >
-                                管理使用者
-                            </Button>
-                        </Link>
-                        <Link
-                            href="/admin/payments"
-                            onClick={handleNavItemClick}
-                            className="py-2 lg:py-0 flex justify-end lg:mx-0 mx-10"
-                        >
-                            <Button variant="destructive" size="sm" className="w-30% lg:w-auto ">
-                                金流查詢
-                            </Button>
-                        </Link>
-                    </>
-                )}
-                <div className="lg:hidden">
-                    {status === "loading" ? (
-                        <span>載入中...</span>
-                    ) : user ? (
-                        <div className="py-2 lg:py-0 flex justify-end lg:mx-0 mx-10">
-                            {/* <span className="text-center">Hi, {user.name}</span> */}
-                            <Button
-                                onClick={async () => {
-                                    await signOut();
-                                    router.push("/");
-                                    setIsMenuOpen(false);
-                                }}
-                                variant="secondary"
-                                size="sm"
-                                className="w-30% lg:w-auto"
-                            >
-                                登出
-                            </Button>
-                        </div>
-                    ) : (
-                        <Button
-                            onClick={() => signIn("google")}
-                            variant="secondary"
-                            size="sm"
-                            className="w-full"
-                        >
-                            使用Gmail登入/註冊
-                        </Button>
-                    )}
-                </div>
-            </div>
-
-            {/* 使用者信息/登入按鈕 - 在桌面版顯示 */}
-            <div className="hidden lg:block">
-                {status === "loading" ? (
-                    <span>載入中...</span>
-                ) : user ? (
-                    <div className="flex items-center gap-4">
-                        <span className="hidden lg:inline">Hi, {user.name}</span>
-                        <Button
-                            onClick={async () => {
-                                await signOut();
-                                router.push("/");
-                            }}
-                            variant="secondary"
-                            size="sm"
-                        >
-                            登出
-                        </Button>
-                    </div>
-                ) : (
-                    <Button onClick={() => signIn("google")} variant="secondary" size="sm">
-                        使用Gmail登入/註冊
-                    </Button>
-                )}
             </div>
         </nav>
     );
