9bff519ec7097ad304677bc38550d2c86ee3e11e
dn070017 <dn070017@gmail.com>
2025-08-15 01:13:43 +0800

    fix typo for supported institute

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -53,7 +53,7 @@ const robotCompetitionOrganizers = {
         { name: "國立中興大學生物產業機電工程學系", url: "#" },
         { name: "國立宜蘭大學生物機電工程學系", url: "#" },
         { name: "國立嘉義大學生物機電工程學系", url: "#" },
-        { name: "國立屏東科技大學生物機電工程學系", url: "#" },
+        { name: "國立屏東科技大學生物機電工程系", url: "#" },
     ],
 };
 
